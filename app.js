d8dacfef71ddeff91c5e1638a905f0b744ce6809
Murray Green <murray.green@warwick.ac.uk>
2021-10-30 19:15:20 +0100

    Added flash messaging and validation

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const { check, validationResult} = require("express-validator");
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 const port = 3000;
@@ -26,12 +29,26 @@ db.on("error", function(err){
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-// body parser stuff
+// body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Set Public Folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Express Session Middleware
+app.use(session({
+  secret: "Keyboard cat",
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require("connect-flash")());
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res);
+  next();
+});
 
 
 // Routes
@@ -48,76 +65,9 @@ app.get('/', function(req, res){
   });
 });
 
-app.get("/articles/add", function(req, res){
-  res.render("add_article", {
-    title:"Add article"
-  });
-});
-
-// Get Single Article
-app.get("/article/:id", function(req, res){
-  Article.findById(req.params.id, function(err, article){
-    res.render("article", {
-      article:article
-    });
-  });
-});
-
-// Load Edit Form
-app.get("/article/edit/:id", function(req, res){
-  Article.findById(req.params.id, function(err, article){
-    res.render("edit_article", {
-      title:"Edit Article",
-      article:article
-    });
-  });
-});
-
-app.post("/articles/add", function(req, res){
-  let article = new Article();
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  article.save(function(err){
-    if(err){
-      console.log(err);
-      return;
-    } else {
-      res.redirect("/");
-    }
-  });
-});
-
-app.post("/articles/edit/:id", function(req, res){
-  let article = {};
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  let query = {_id:req.params.id}
-
-  Article.update(query, article, function(err){
-    if(err){
-      console.log(err);
-      return;
-    } else {
-      res.redirect("/");
-    }
-  });
-});
-
-// Delete Article
-app.delete("/article/:id", function(req, res){
-  let query = {_id:req.params.id}
-
-  Article.remove(query, function(err){
-    if(err){
-      console.log(err);
-    }
-    res.send("Success");
-  });
-});
+// Route Files
+let articles = require("./routes/articles");
+app.use("/articles", articles);
 
 
 // Start
---
+++ routes/articles.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult} = require("express-validator");
+
+//Bring in Article model
+let Article = require("../models/article");
+
+
+// Add article
+router.get("/add", function(req, res){
+  res.render("add_article", {
+    title:"Add article"
+  });
+});
+
+// Get Single Article
+router.get("/:id", function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render("article", {
+      article:article
+    });
+  });
+});
+
+// Load Edit Form
+router.get("/edit/:id", function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render("edit_article", {
+      title:"Edit Article",
+      article:article
+    });
+  });
+});
+
+router.post("/add", [
+  check("title", "Title must not be empty").isLength({ min: 1 }),
+  check("author", "Author must not be empty").isLength({ min: 1 }),
+  check("body", "Body must not be empty").isLength({ min: 1 })
+  ], (req, res) => {
+
+  //Get errors
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+        res.render('add_article', {
+            title: 'Add Article',
+            errors: errors
+        });
+  } else {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function(err){
+      if(err){
+        console.log(err);
+        return;
+      } else {
+        req.flash("success","Article Added");
+        res.redirect("/");
+      }
+    });
+  }
+});
+
+router.post("/edit/:id", function(req, res){
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      req.flash("success","Article Updated");
+      res.redirect("/");
+    }
+  });
+});
+
+// Delete Article
+router.delete("/:id", function(req, res){
+  let query = {_id:req.params.id}
+
+  Article.remove(query, function(err){
+    if(err){
+      console.log(err);
+    }
+    res.send("Success");
+  });
+});
+
+module.exports = router;
